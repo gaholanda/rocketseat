ea194f5f9c10e19db5eece668ff36c44e93bb347
Glauber Holanda <glauberholanda10@gmail.com>
2022-03-17 15:20:19 -0300

    feat(ignite): chapter one - module three

--- 01-github-explorer/src/components/RepositoryItem.jsx
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -5,7 +5,7 @@ export function RepositoryItem(props) {
     <li>
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
-      <a href={props.repository.link}>Acessar</a>
+      <a href={props.repository.html_url}>Acessar</a>
     </li>
   )
 }
