217681a415ef4556f09c31e93520c4ba452aa2db
Glauber Holanda <glauberholanda10@gmail.com>
2022-03-19 15:30:00 -0300

    feat(ignite): challenge 02 - add sidebar

--- ignite-01-challenge-02/src/App.tsx
+++ ignite-01-challenge-02/src/App.tsx
@@ -1,15 +1,11 @@
 import { useEffect, useState } from "react";
 
-import { Button } from "./components/Button";
-
-// import { SideBar } from './components/SideBar';
+import { SideBar } from './components/SideBar';
 import { Content } from "./components/Content";
 
 import { api } from "./services/api";
 
 import "./styles/global.scss";
-
-import "./styles/sidebar.scss";
 
 interface GenreResponseProps {
   id: number;
@@ -17,23 +13,10 @@ interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-
   const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
-  const [movies, setMovies] = useState<MovieProps[]>([]);
+  
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
     {} as GenreResponseProps
   );
@@ -45,12 +28,6 @@ export function App() {
   }, []);
 
   useEffect(() => {
-    api
-      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
-      .then((response) => {
-        setMovies(response.data);
-      });
-
     api
       .get<GenreResponseProps>(`genres/${selectedGenreId}`)
       .then((response) => {
@@ -64,31 +41,8 @@ export function App() {
 
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
-      <nav className="sidebar">
-        <span>
-          Watch<p>Me</p>
-        </span>
-
-        <div className="buttons-container">
-          {genres.map((genre) => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-      </nav>
-      <div className="container">
-        <header>
-          <span className="category">
-            Categoria:<span> {selectedGenre.title}</span>
-          </span>
-        </header>
-        <Content movies={movies} />
-      </div>
+      <SideBar genres={genres} handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} />
+      <Content selectedGenreTitle={selectedGenre.title} selectedGenreId={selectedGenreId} />
     </div>
   );
 }
--- ignite-01-challenge-02/src/components/Content.tsx
+++ ignite-01-challenge-02/src/components/Content.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
 import { MovieCard } from "./MovieCard";
 
-import '../styles/content.scss';
+import "../styles/content.scss";
 
 interface MovieProps {
   imdbID: string;
@@ -14,23 +16,42 @@ interface MovieProps {
 }
 
 interface ContentProps {
-  movies: MovieProps[];
+  selectedGenreTitle: string;
+  selectedGenreId: number;
 }
 
-export function Content({ movies }: ContentProps) {
+export function Content({ selectedGenreTitle, selectedGenreId }: ContentProps) {
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+  }, [selectedGenreId]);
+
   return (
-    <main>
-      <div className="movies-list">
-        {movies.map((movie) => (
-          <MovieCard
-            key={movie.imdbID}
-            title={movie.Title}
-            poster={movie.Poster}
-            runtime={movie.Runtime}
-            rating={movie.Ratings[0].Value}
-          />
-        ))}
-      </div>
-    </main>
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenreTitle}</span>
+        </span>
+      </header>
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
   );
 }
