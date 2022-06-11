b98b6e5b4f3f07e7106c806b1c31a35c8a467b8d
Glauber Holanda <glauberholanda10@gmail.com>
2022-06-11 15:33:46 -0300

    feat(ignite): chapter three, module two, section three

--- ignite-03-ig-news/src/components/SubscribeButton/index.tsx
+++ ignite-03-ig-news/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
+import { signIn, useSession } from "next-auth/react";
 import React from "react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 import styles from "./SubscribeButton.module.scss";
 
@@ -7,10 +10,30 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+  const { status } = useSession();
+
+  async function handleSubscribe() {
+    if(status === "unauthenticated") {
+      signIn('github')
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+      const stripeJs = await getStripeJs();
+      stripeJs.redirectToCheckout({ sessionId });
+    } catch(err) {
+      alert(err.message)
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
